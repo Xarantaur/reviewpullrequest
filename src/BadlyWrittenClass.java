cf5a957350477cda96069e679f8ac16e5dfe08ba
Xarantaur <115454990+Xarantaur@users.noreply.github.com>
2023-09-08 11:02:53 +0200

    Update BadlyWrittenClass.java

--- src/BadlyWrittenClass.java
+++ src/BadlyWrittenClass.java
@@ -13,19 +13,10 @@ public class BadlyWrittenClass {
 
     // A method to check if the number is even
     public boolean isEven() {
-        if (number % 2 == 0)
-            return true;
-        else
-            return false;
+        return (number % 2 == 0) ? true : false;
     }
 
     // A method to check if the number is odd
-    public boolean isOdd() {
-        if (number % 2 != 0)
-            return true;
-        else
-            return false;
-    }
 
     // A method to check if the number is prime
     public boolean isPrime() {
