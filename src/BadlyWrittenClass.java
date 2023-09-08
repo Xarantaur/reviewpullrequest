0814dd2b86ddde67eaa1896408384a1efbbb23a6
Xarantaur <115454990+Xarantaur@users.noreply.github.com>
2023-09-08 09:13:44 +0200

    Initial commit

---
+++ src/BadlyWrittenClass.java
@@ -0,0 +1,49 @@
+public class BadlyWrittenClass {
+    private int number;
+
+    // Constructor with multiple parameters
+    public BadlyWrittenClass(int n) {
+        number = n;
+    }
+
+    // Get the number
+    public int getNumber() {
+        return number;
+    }
+
+    // A method to check if the number is even
+    public boolean isEven() {
+        if (number % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+
+    // A method to check if the number is odd
+    public boolean isOdd() {
+        if (number % 2 != 0)
+            return true;
+        else
+            return false;
+    }
+
+    // A method to check if the number is prime
+    public boolean isPrime() {
+        if (number < 2)
+            return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    // A method to calculate the factorial of the number
+    public int calculateFactorial() {
+        int result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
